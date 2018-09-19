46858fe971f4eb4a05b5b038e3d10b421f5def67
lexBogos <lex.bogos@gmail.com>
2018-09-19 18:30:26 +0300

    complete_solution_sorter

--- src/index.js
+++ src/index.js
@@ -1,30 +1,73 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
+    this.additional_array = [];
+    this.k = 0; 
+    this.l = 0;
+
   }
 
   add(element) {
-    // your implementation
+    this.array[this.array.length]=element
+    return this.array
   }
 
   at(index) {
-    // your implementation
+    return this.array[index]
+  
   }
 
   get length() {
-    // your implementation
+    return this.array.length
+
   }
 
   toArray() {
-    // your implementation
+    return this.array
+
   }
 
   sort(indices) {
-    // your implementation
-  }
+
+    console.log('Before use function', this.array)
+    indices.sort()
+     for (this.k=indices.length; this.k > 0; this.k=this.k-1)
+    { 
+    this.additional_array[this.additional_array.length]=this.array[indices[indices.length-this.k]] 
+    }
+
+    if (this.l != 0){
+    
+    this.additional_array.sort(this.l)
+
+    }
+     else 
+    {
+      this.additional_array.sort(function(a,b){
+        return a-b;
+      })
+
+    }
+      console.log(this.additional_array)
+
+     for (this.k=indices.length; this.k > 0; this.k=this.k-1)
+    {
+      
+      this.array[indices[indices.length-this.k]]=this.additional_array[indices.length-this.k]
+    }
+      this.additional_array = []
+    console.log(this.array)
+    return this.array
+
+    }
+
+
 
   setComparator(compareFunction) {
-    // your implementation
+  
+  this.l=compareFunction
+  return this.l
+           
   }
 }
 
